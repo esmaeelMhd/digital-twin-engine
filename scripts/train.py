97b4ec9d6a57e18d3546f99d06e961d714f289af
esmaeelMhd <esm.moh2024@gmail.com>
2026-03-28 14:29:32 +0100

    Increase peak_lr to 2.0e-3 by injecting it into the config dict to further accelerate convergence, building on the success of the 1.0e-3 experiment.

--- scripts/train.py
+++ scripts/train.py
@@ -106,6 +106,7 @@ def main():
     with open(args.config, "r") as f:
         config = yaml.safe_load(f)
     config.setdefault("model", {})["initial_diffusion_scale"] = 0.0001
+    config.setdefault("training", {})["peak_lr"] = 2.0e-3
     
     with open(args.cstr_config, "r") as f:
         cstr_config = yaml.safe_load(f)
